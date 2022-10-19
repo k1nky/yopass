547a42d6cdb9fbac9879dad85710e74c9d784555
Andrey Shalashov <avshalashov@yandex.ru>
2022-10-19 18:19:51 +0300

    place auth to a separate module

---
+++ pkg/auth/auth.go
@@ -0,0 +1,122 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"gopkg.in/yaml.v3"
+)
+
+type User struct {
+	Username string
+	Password string
+	Role     string
+}
+
+type JwtAuthOptions struct {
+	Secret []byte
+	Users  []User
+}
+
+type JwtAuth struct {
+	Path    string
+	Options *JwtAuthOptions
+}
+
+func GenerateKey(length int) (string, error) {
+	b := make([]byte, length)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b)[:length], nil
+}
+
+func (auth *JwtAuth) Load(filename string) (err error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	auth.Path = filename
+	auth.Options = &JwtAuthOptions{}
+	err = yaml.NewDecoder(f).Decode(auth.Options)
+	return
+}
+
+func (auth *JwtAuth) Save() error {
+	f, err := os.Create(auth.Path)
+	if err != nil {
+		return err
+	}
+
+	return yaml.NewEncoder(f).Encode(auth.Options)
+}
+
+func NewJwtAuth(filename string, adminName string) *JwtAuth {
+	secret, _ := GenerateKey(64)
+	password, _ := GenerateKey(10)
+	return &JwtAuth{
+		Path: filename,
+		Options: &JwtAuthOptions{
+			Secret: []byte(secret),
+			Users: []User{
+				{
+					Username: adminName,
+					Password: password,
+					Role:     "admin",
+				},
+			},
+		},
+	}
+}
+
+func (auth *JwtAuth) ValidateUser(user *User) bool {
+	for _, u := range auth.Options.Users {
+		if u.Username == user.Username && u.Password == user.Password {
+			user.Role = u.Role
+			return true
+		}
+	}
+	return false
+}
+
+func (auth *JwtAuth) GenerateToken(u User) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"role": u.Role,
+		"name": u.Username,
+		"exp":  time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(auth.Options.Secret)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return tokenString
+}
+
+func (auth *JwtAuth) AuthorizeRequest(r *http.Request) (*User, error) {
+	if r.Header["Token"] == nil {
+		return nil, fmt.Errorf("no auth token found")
+	}
+	tokenString := r.Header["Token"][0]
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("there was an error in parsing")
+		}
+		return auth.Options.Secret, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("invalid token")
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return &User{
+			Username: claims["name"].(string),
+			Role:     claims["role"].(string),
+		}, nil
+	}
+	return nil, fmt.Errorf("unauthorized token")
+}
